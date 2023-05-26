a5b77fc97ef8db2147306ffe3e882bfc226bc45d
devimtiaz <devsabbirhossin@gmail.com>
2023-05-27 01:42:37 +0600

    Git Hub File Add

---
+++ main.js
@@ -0,0 +1,26 @@
+/*
+Create a function that will return children, teenagers, young people, old people from the age
+
+Create a function that will return the area of a rectangle, square, triangle
+
+GPA, CGPA, GRADE function for result publishing
+
+Create an age calculator function
+
+Create a BMI function for health
+
+Create a currency converter function from taka to USD, CAD, POUND, EURO etc
+
+
+একটি ফাংশন তৈরি করুন যা শিশু, কিশোর, যুবক, বৃদ্ধ বয়স থেকে ফিরে আসবে
+
+একটি ফাংশন তৈরি করুন যা একটি আয়তক্ষেত্র, বর্গক্ষেত্র, ত্রিভুজের ক্ষেত্রফল প্রদান করবে
+
+ফলাফল প্রকাশের জন্য GPA, CGPA, GRADE ফাংশন
+
+একটি বয়স ক্যালকুলেটর ফাংশন তৈরি করুন
+
+স্বাস্থ্যের জন্য একটি BMI ফাংশন তৈরি করুন
+
+টাকা থেকে USD, CAD, POUND, EURO ইত্যাদিতে একটি মুদ্রা রূপান্তরকারী ফাংশন তৈরি করুন
+**/
